9309c86a70b2e3cd7a3832a1c8d19af2c5e37485
MAHANMALPORU734 <mahan.navidu@gmail.com>
2021-03-06 10:31:22 +0530

    Hello world

---
+++ Cafe-NIBM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cafe-NIBM
+//
+//  Created by Mahan Navidu on 2021-03-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cafe-NIBMTests/Cafe_NIBMTests.swift
@@ -0,0 +1,33 @@
+//
+//  Cafe_NIBMTests.swift
+//  Cafe-NIBMTests
+//
+//  Created by Mahan Navidu on 2021-03-06.
+//
+
+import XCTest
+@testable import Cafe_NIBM
+
+class Cafe_NIBMTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
