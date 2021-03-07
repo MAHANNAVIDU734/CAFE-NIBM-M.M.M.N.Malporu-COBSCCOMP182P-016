c96c40fe5f4e950dd47d7bfafa3ce752d01ba000
MAHANMALPORU734 <mahan.navidu@gmail.com>
2021-03-07 16:29:38 +0530

    sppermissons location view controller

--- Cafe-NIBM/LocationViewController.swift
+++ Cafe-NIBM/LocationViewController.swift
@@ -22,5 +22,18 @@ class LocationViewController: UIViewController {
         controller.present(on: self)
         
     }
+    func deniedData(for permission: SPPermission) -> SPPermissionDeniedAlertData? {
+        if permission == .notification {
+            let data = SPPermissionDeniedAlertData()
+            data.alertOpenSettingsDeniedPermissionTitle = "Permission denied"
+            data.alertOpenSettingsDeniedPermissionDescription = "Please, go to Settings and allow permission."
+            data.alertOpenSettingsDeniedPermissionButtonTitle = "Settings"
+            data.alertOpenSettingsDeniedPermissionCancelTitle = "Cancel"
+            return data
+        } else {
+            // If returned nil, alert will not show.
+            return nil
+        }
+    }
     
 }
